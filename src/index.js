40c7d3a5300d1907cd9898b4fc6d02789fa5a5b5
Shihab AL-Mekhlafi <shihabalmekhlafy@gmail.com>
2020-07-21 22:17:18 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    // your code here
+         event.preventDefault();
+         document.getElementById("submit").addEventListener("click", function (event) {
+        let task = document.getElementById("new-task-description").value;
+        document.getElementById("new-task-description").value = '';
+        let newElement = document.createElement('li');
+        document.getElementById("tasks").appendChild(newElement);
+        newElement.innerHTML = task + " <button class='btn'>X</button>";
+        newElement.addEventListener('click', function () {
+            this.parentNode.removeChild(this);
+        });
+
+
+    });
 });
