4af73709fe9f964e78a379ff7bcb6d49b3eae8fc
Shihab AL-Mekhlafi <shihabalmekhlafy@gmail.com>
2020-07-24 20:58:55 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     // your code here
-         event.preventDefault();
-         document.getElementById("submit").addEventListener("click", function (event) {
-        let task = document.getElementById("new-task-description").value;
-        document.getElementById("new-task-description").value = '';
-        let newElement = document.createElement('li');
+
+    document.getElementById("submit").addEventListener("click", function (event) {
+	    event.preventDefault();
+	    let task = document.getElementById("new-task-description").value;
+		task.value = '';
+	    let newElement = document.createElement('li');
         document.getElementById("tasks").appendChild(newElement);
         newElement.innerHTML = task + " <button class='btn'>X</button>";
         newElement.addEventListener('click', function () {
